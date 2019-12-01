ed8cf6be7abc92a12b4814da383747ee660c8814
doz <dozlouka@gmail.com>
2019-12-01 18:20:42 +0100

    lecture

--- src/editor/FileReader/EnigmaFilesManagement.java
+++ src/editor/FileReader/EnigmaFilesManagement.java
@@ -1,13 +1,17 @@
 package editor.FileReader;
 
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
 import editor.Enigma.Advice;
-import editor.Enigma.Condition.Activated;
-import editor.Enigma.Condition.Condition;
-import editor.Enigma.Condition.HaveInHands;
+import editor.Enigma.Condition.*;
 import editor.Enigma.Enigma;
+import editor.Enigma.Operation.Give;
 import editor.Enigma.Operation.Operation;
 import editor.Enigma.Operation.Summon;
 import editor.Enigma.Operation.Unlock;
+import editor.Entity.Interface.Entity;
+import editor.Entity.Item.Activatable;
 import editor.Entity.Item.Door;
 import editor.Entity.Item.Switch;
 import editor.Entity.Player.Player;
@@ -16,6 +20,8 @@ import editor.map.Case;
 import org.lwjgl.Sys;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 
 /**
@@ -34,6 +40,11 @@ public class EnigmaFilesManagement {
      * Ecrivain
      */
     private BufferedWriter writer;
+
+    /**
+     * Lecteur
+     */
+    private BufferedReader reader;
 
     /**
      * Syntaxe d'une indentation
@@ -168,7 +179,7 @@ public class EnigmaFilesManagement {
                         if (!firstRound) this.writer.write(",");
                         else firstRound = false;
                         this.writer.newLine();
-                        if (oAtt.equals(OperationAttributes.PATH)) this.writer.write(this.getIndentation() + "\"" + NOT_ATTRIBUTE_BEFORE_SYNTAX + oAtt + "\"" + CLASSIC_ATTRIBUTE_SYNTAX + "\"" + operationsAtt.get(oAtt) + "\"");
+                        if (oAtt.equals(OperationAttributes.PATH)) this.writer.write(this.getIndentation() + "\"" + NOT_ATTRIBUTE_BEFORE_SYNTAX + oAtt.toLowerString() + "\"" + CLASSIC_ATTRIBUTE_SYNTAX + "\"" + operationsAtt.get(oAtt) + "\"");
                         else this.writer.write(this.getIndentation() + "\"" + oAtt.toLowerString() + "\"" + CLASSIC_ATTRIBUTE_SYNTAX + "\"" + operationsAtt.get(oAtt) + "\"");
                     }
                     break;
@@ -177,7 +188,7 @@ public class EnigmaFilesManagement {
                         if (!firstRound) this.writer.write(",");
                         else firstRound = false;
                         this.writer.newLine();
-                        if (sAtt.equals(SummonAttributes.PATH)) this.writer.write(this.getIndentation() + "\"" + NOT_ATTRIBUTE_BEFORE_SYNTAX + sAtt + "\"" + CLASSIC_ATTRIBUTE_SYNTAX + "\"" + operationsAtt.get(sAtt) + "\"");
+                        if (sAtt.equals(SummonAttributes.PATH)) this.writer.write(this.getIndentation() + "\"" + NOT_ATTRIBUTE_BEFORE_SYNTAX + sAtt.toLowerString() + "\"" + CLASSIC_ATTRIBUTE_SYNTAX + "\"" + operationsAtt.get(sAtt) + "\"");
                         else this.writer.write(this.getIndentation() + "\"" + sAtt.toLowerString() + "\"" + CLASSIC_ATTRIBUTE_SYNTAX + "\"" + operationsAtt.get(sAtt) + "\"");
                     }
                     break;
@@ -274,63 +285,250 @@ public class EnigmaFilesManagement {
         this.indentation--;
         this.writer.write(this.getIndentation() + END_OBJECT_SYNTAX);
 
-
         this.writer.close();
     }
 
     /**
-     *
-     * @param filePath
-     * @return
-     * @throws IOException
-     * @throws NumberFormatException
-     * @throws IllegalStateException
-     * @throws IllegalArgumentException
-     * @throws ClassNotFoundException
+     * Extrait le nom d'un attribut depuis une ligne lue dans un fichier
+     * @param read Ligne lue
+     * @return Le nom de l'attribut, une chaine vide si la ligne ne concerne pas le nom d'un attribut
+     */
+    private String extractAttributeName(String read){
+        if(read.contains(CLASSIC_ATTRIBUTE_SYNTAX) && !read.contains(NEW_CLASS_TAB_SYNTAX)) return read.substring(0,read.indexOf(CLASSIC_ATTRIBUTE_SYNTAX)).replaceAll("\"", "");
+        else return "";
+    }
+
+    /**
+     * Extrait le nom d'une classe depuis une ligne lue dans un fichier
+     * @param read Ligne lue
+     * @return Le nom de la classe, une chaine vide si la ligne ne concerne pas le nom d'une classe
+     */
+    private String extractClassName(String read){
+        if(read.contains(NEW_CLASS_TAB_SYNTAX)) return read.substring(0,read.indexOf(NEW_CLASS_TAB_SYNTAX)).replaceAll("\"", "");
+        else return "";
+    }
+
+    /**
+     * Extrait la valeur d'un attribut depuis une ligne lue dans un fichier
+     * @param read Ligne lue
+     * @return La valeur de l'attribut, une chaine si la ligne ne concerne pas la valeur d'un attribut
+     */
+    private String extractAttributeValue(String read){
+
+        if(read.contains(CLASSIC_ATTRIBUTE_SYNTAX)) return read.replaceAll("\"", "").replaceAll(",", "").substring(read.indexOf(CLASSIC_ATTRIBUTE_SYNTAX)).replace(CLASSIC_ATTRIBUTE_SYNTAX,"");
+        else return "";
+    }
+
+    /**
+     * Lit les indices et les instancie
+     * @return Les indices instanciés
+     * @throws IOException En cas d'erreur de lecture
      */
     @SuppressWarnings("unchecked")
-    public ArrayList<Enigma> readEnigmas(String filePath) throws IOException, ClassNotFoundException {
+    private ArrayList<Advice> readAdvices(int line) throws IOException {
+
+        ArrayList<Advice> advices = new ArrayList<Advice>();
+        String read;
+        int braceCount = 0;
+        HashMap<AdviceAttributes,String> aAtt = new HashMap<AdviceAttributes,String>();
+
+        while ((read = this.reader.readLine()) != null) {
+            line++;
+            read = read.trim();
+            String name = this.extractAttributeName(read);
+
+            if (!name.equals("")){
+                if(name.equals(AdviceAttributes.ADVICE.toLowerString())) aAtt.put(AdviceAttributes.ADVICE,this.extractAttributeValue(read));
+                if(name.equals(AdviceAttributes.DELAY.toLowerString())) aAtt.put(AdviceAttributes.DELAY,this.extractAttributeValue(read));
+            }else{
+                if (read.equals(NEW_OBJECT_SYNTAX)) braceCount++;
+
+                if(read.contains(END_OBJECT_SYNTAX)){
+                    braceCount--;
+                    if(braceCount == 0) advices.add(new Advice(aAtt.get(AdviceAttributes.ADVICE),Integer.parseInt(aAtt.get(AdviceAttributes.DELAY))));
+                    if(!read.contains(END_OBJECT_SYNTAX + ",")) break;
+                }
+            }
+        }
+
+        if (braceCount > 0) throw new IllegalStateException("Le fichier n'est pas dans un format valide. Accolade fermante manquante");
+        if (braceCount < 0) throw new IllegalStateException("Le fichier n'est pas dans un format valide. Accolade ouvrante manquante");
+
+        return (ArrayList<Advice>)advices.clone();
+    }
+
+    /**
+     * Lit les conditions et les instancie
+     * @return Les conditions instanciées
+     * @throws IOException En cas d'erreur de lecture
+     */
+    @SuppressWarnings("unchecked")
+    private ArrayList<Condition> readConditions(int line) throws IOException {
+
+        ArrayList<Condition> conditions = new ArrayList<Condition>();
+        String read;
+        String className = "";
+        int braceCount = 0;
+        HashMap<ConditionAttributes,String> cAtt = new HashMap<ConditionAttributes,String>();
+
+        while ((read = this.reader.readLine()) != null) {
+            line++;
+            read = read.trim();
+            String name = this.extractAttributeName(read);
+
+            if (!name.equals("")){
+                if(name.equals(NOT_ATTRIBUTE_BEFORE_SYNTAX + ConditionAttributes.PATH.toLowerString())) {
+                    String[] split = this.extractAttributeValue(read).split("\\.");
+                    className = split[split.length - 1];
+                }
+                if(name.equals(AdviceAttributes.ADVICE.toLowerString())) cAtt.put(ConditionAttributes.ENTITY,this.extractAttributeValue(read));
+            }else{
+                if (read.equals(NEW_OBJECT_SYNTAX)) braceCount++;
+
+                if(read.contains(END_OBJECT_SYNTAX)){
+                    braceCount--;
+                    if(braceCount == 0){
+
+                        switch (className){
+                            case "Activated":
+                                conditions.add(new Activated(new Switch()));
+                                break;
+                            case "Answer":
+                                conditions.add(new Answer());
+                                break;
+                            case "HaveInHands":
+                                conditions.add(new HaveInHands(new Switch()));
+                                break;
+                            case "HaveInInventory":
+                                conditions.add(new HaveInInventory(new Switch()));
+                                break;
+                        }
+                    }
+                    if(!read.contains(END_OBJECT_SYNTAX + ",")) break;
+                }
+            }
+        }
+
+        if (braceCount > 0) throw new IllegalStateException("Le fichier n'est pas dans un format valide. Accolade fermante manquante");
+        if (braceCount < 0) throw new IllegalStateException("Le fichier n'est pas dans un format valide. Accolade ouvrante manquante");
+
+        return (ArrayList<Condition>)conditions.clone();
+    }
+
+    /**
+     * Lit les opérations et les instancie
+     * @return Les opérations instanciées
+     * @throws IOException En cas d'erreur de lecture
+     */
+    @SuppressWarnings("unchecked")
+    private ArrayList<Operation> readOperations(int line) throws IOException {
+
+        ArrayList<Operation> operations = new ArrayList<Operation>();
+        String read;
+        String className = "";
+        int braceCount = 0;
+        HashMap<OperationAttributes,String> oAtt = new HashMap<OperationAttributes,String>();
+
+        while ((read = this.reader.readLine()) != null) {
+            line++;
+            read = read.trim();
+            String name = this.extractAttributeName(read);
+
+            if (!name.equals("")){
+                if(name.equals(NOT_ATTRIBUTE_BEFORE_SYNTAX + OperationAttributes.PATH.toLowerString())) {
+                    String[] split = this.extractAttributeValue(read).split("\\.");
+                    className = split[split.length - 1];
+                }
+                if(name.equals(AdviceAttributes.ADVICE.toLowerString())) oAtt.put(OperationAttributes.ENTITY,this.extractAttributeValue(read));
+            }else{
+                if (read.equals(NEW_OBJECT_SYNTAX)) braceCount++;
+
+                if(read.contains(END_OBJECT_SYNTAX)){
+                    braceCount--;
+                    if(braceCount == 0){
+
+                        switch (className){
+                            case "Give":
+                                operations.add(new Give(new Switch()));
+                                break;
+                            case "Unlock":
+                                operations.add(new Unlock(new Door()));
+                                break;
+                            case "Summon":
+                                operations.add(new Summon(new Player(),new Case()));
+                                break;
+                        }
+                    }
+                    if(!read.contains(END_OBJECT_SYNTAX + ",")) break;
+                }
+            }
+        }
+
+        if (braceCount > 0) throw new IllegalStateException("Le fichier n'est pas dans un format valide. Accolade fermante manquante");
+        if (braceCount < 0) throw new IllegalStateException("Le fichier n'est pas dans un format valide. Accolade ouvrante manquante");
+
+        return (ArrayList<Operation>)operations.clone();
+    }
+
+    /**
+     * Lit un fichier json d'énigmes et les instancie
+     * @param filePath Chemin du fichier
+     * @return Les énigmes instannciées
+     * @throws IOException En cas d'erreur de lecture
+     * @throws IllegalArgumentException Si le fichier n'est pas un ".json"
+     */
+    @SuppressWarnings("unchecked")
+    public ArrayList<Enigma> readEnigmas(String filePath) throws IOException {
         if(!filePath.endsWith(".json")) throw new IllegalArgumentException("Le fichier n'est pas dans un format valide \".json\"");
 
         ArrayList<Enigma> enigmas = new ArrayList<Enigma>();
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        this.reader = new BufferedReader(new FileReader(filePath));
         String read;
-        String filtred = "";
-        String value;
+        ArrayList<Advice> advices = null;
+        ArrayList<Condition> conditions = new ArrayList<Condition>();
+        ArrayList<Operation> operations = new ArrayList<Operation>();
         int braceCount = 0;
         int bracketCount = 0;
         int line = 0;
         HashMap<EnigmaAttributes,String> eAtt = new HashMap<EnigmaAttributes,String>();
 
-        if(reader.readLine().trim().equals(NEW_OBJECT_SYNTAX)){
+        if(this.reader.readLine().trim().equals(NEW_OBJECT_SYNTAX)){
             braceCount++;
             line++;
-            read = reader.readLine().replaceAll("\"", "").trim();
-            if(read.contains(NEW_CLASS_TAB_SYNTAX) && read.substring(0,read.indexOf(NEW_CLASS_TAB_SYNTAX)).equals("Enigmas")) {
+
+            if(this.extractClassName(this.reader.readLine().trim()).equals("Enigmas")) {
                 bracketCount++;
                 line++;
-                while ((read = reader.readLine()) != null) {
+
+                while ((read = this.reader.readLine()) != null) {
+                    read = read.trim();
                     line++;
-                    read = read.replaceAll("\"", "").replaceAll(",", "").trim();
+                    String name;
 
-                    if (read.equals(NEW_OBJECT_SYNTAX)) braceCount++;
-                    if (read.equals(END_CLASS_TAB_SYNTAX)) bracketCount--;
-                    if (read.equals(END_OBJECT_SYNTAX)) {
-                        braceCount--;
-                        System.out.println(eAtt.get(EnigmaAttributes.TITLE)+" "+eAtt.get(EnigmaAttributes.DESCRIPTION)+" "+Boolean.parseBoolean(eAtt.get(EnigmaAttributes.KNOWN))+" "+Integer.parseInt(eAtt.get(EnigmaAttributes.CURRENT_ADVICE_INDEX)));
-                        if(braceCount == 1 && bracketCount == 1) enigmas.add(new Enigma(eAtt.get(EnigmaAttributes.TITLE),eAtt.get(EnigmaAttributes.DESCRIPTION),Boolean.parseBoolean(eAtt.get(EnigmaAttributes.KNOWN)),Integer.parseInt(eAtt.get(EnigmaAttributes.CURRENT_ADVICE_INDEX)),null,null,null));
-                    }
+                    if(!(name = this.extractAttributeName(read)).equals("")){
+                        String value = this.extractAttributeValue(read);
+                        if(name.equals(EnigmaAttributes.TITLE.toLowerString())) eAtt.put(EnigmaAttributes.TITLE,value);
+                        if(name.equals(EnigmaAttributes.DESCRIPTION.toLowerString())) eAtt.put(EnigmaAttributes.DESCRIPTION,value);
+                        if(name.equals(EnigmaAttributes.KNOWN.toLowerString())) eAtt.put(EnigmaAttributes.KNOWN,value);
+                        if(name.equals(EnigmaAttributes.CURRENT_ADVICE_INDEX.toLowerString())) eAtt.put(EnigmaAttributes.CURRENT_ADVICE_INDEX,value);
 
-                    if (read.contains(NEW_CLASS_TAB_SYNTAX)) {
-                        bracketCount++;
-                    }else if(read.contains(CLASSIC_ATTRIBUTE_SYNTAX)) {
-                        filtred = read.substring(0,read.indexOf(CLASSIC_ATTRIBUTE_SYNTAX));
-                        value = read.substring(read.indexOf(CLASSIC_ATTRIBUTE_SYNTAX)).replace(":","").trim();
+                    }else {
+                        if (!(name = this.extractClassName(read)).equals("")) {
+                            bracketCount++;
 
-                        if(filtred.equals(EnigmaAttributes.TITLE.toLowerString())) eAtt.put(EnigmaAttributes.TITLE,value);
-                        if(filtred.equals(EnigmaAttributes.DESCRIPTION.toLowerString())) eAtt.put(EnigmaAttributes.DESCRIPTION,value);
-                        if(filtred.equals(EnigmaAttributes.KNOWN.toLowerString())) eAtt.put(EnigmaAttributes.KNOWN,value);
-                        if(filtred.equals(EnigmaAttributes.CURRENT_ADVICE_INDEX.toLowerString())) eAtt.put(EnigmaAttributes.CURRENT_ADVICE_INDEX,value);
+                            if (name.equals(EnigmaAttributes.ADVICES.toLowerString())) advices = this.readAdvices(line);
+                            if (name.equals(EnigmaAttributes.CONDITIONS.toLowerString())) conditions = this.readConditions(line);
+                            if (name.equals(EnigmaAttributes.OPERATIONS.toLowerString())) operations = this.readOperations(line);
+
+                        } else {
+                            if (read.contains(NEW_OBJECT_SYNTAX)) braceCount++;
+                            if (read.contains(END_CLASS_TAB_SYNTAX)) bracketCount--;
+                            if (read.contains(END_OBJECT_SYNTAX)) {
+                                braceCount--;
+                                if (braceCount == 1 && bracketCount == 1)
+                                    enigmas.add(new Enigma(eAtt.get(EnigmaAttributes.TITLE), eAtt.get(EnigmaAttributes.DESCRIPTION), Boolean.parseBoolean(eAtt.get(EnigmaAttributes.KNOWN)), Integer.parseInt(eAtt.get(EnigmaAttributes.CURRENT_ADVICE_INDEX)), advices, conditions, operations));
+                            }
+                        }
                     }
                 }
 
@@ -367,12 +565,12 @@ public class EnigmaFilesManagement {
         enigmas.add(e2);
 
         try {
-            /*efw.writeEnigmas("TestWrite/write.json",enigmas)*/
+            efw.writeEnigmas("TestWrite/write.json",enigmas);
            enigmas = efw.readEnigmas("TestWrite/write.json");
             for (Enigma en: enigmas) {
-                System.out.println(en);
+                System.out.println(en.toLongString());
             }
-        }catch(IOException | ClassNotFoundException ex){
+        }catch(IOException ex){
             System.err.println("oups"+ex.getMessage());
         }
     }
