ffd39cd3e5de580bf9a8c7fd2f6c3bc25d8f8b42
louka <dozlouka@gmail.com>
2020-02-01 15:50:22 +0100

    export

--- src/modifs/MapData.java
+++ src/modifs/MapData.java
@@ -3,31 +3,84 @@ package modifs;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Donnée propre à une map
+ *
+ * @author Jorys-Micke ALAÏS
+ * @author Louka DOZ
+ * @author Loic SENECAT
+ * @author Quentin RAMSAMY-AGEORGES
+ * @version 6.0 01 fevrier 2020
+ * @since 6.0
+ */
 public class MapData {
+    /**
+     * Auteur de la map
+     * Donné à la création de la map, il ne peut être changé plus tard
+     */
     private String author;
+    /**
+     * Nom de la map
+     */
     private String name;
+    /**
+     * Description de la map
+     */
     private String description;
+    /**
+     * Durée
+     */
     private int duration;
+    /**
+     * Nombre de joueurs maximum
+     */
     private int maxPlayers;
+    /**
+     * Est multijoueurs
+     */
     private boolean isMultiPlayer;
 
+    /**
+     * Attribut auteur
+     */
     public final static String AUTHOR = "author";
+    /**
+     * Attribut description
+     */
     public final static String DESCRIPTION = "description";
+    /**
+     * Attribut nom
+     */
     public final static String NAME = "name";
+    /**
+     * Attribut durée
+     */
     public final static String DURATION = "duration";
+    /**
+     * Attribut nombre de joueurs maximum
+     */
     public final static String MAX_PLAYERS = "maxPLayers";
+    /**
+     * Attribut est multijoueurs
+     */
     public final static String MULTI_PLAYERS = "multiPlayers";
 
-    public MapData(){
-        this.author = "non author";
+    /**
+     * @param author Auteur
+     */
+    public MapData(String author){
+        this.author = author;
         this.name = "unnamed";
-        this.name = "";
+        this.description = "";
         this.duration = 5;
         this.maxPlayers = 1;
         this.isMultiPlayer = false;
     }
 
-    public void initFromData(HashMap<String,String> data){
+    /**
+     * @param data Valeurs des attributs
+     */
+    public MapData(HashMap<String,String> data){
         ArrayList<String> dataName = new ArrayList<>();
         dataName.add(AUTHOR);
         dataName.add(DESCRIPTION);
@@ -64,54 +117,96 @@ public class MapData {
         }
     }
 
+    /**
+     * Définir la description
+     * @param description Description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Définir la durée
+     * @param duration Durée
+     */
     public void setDuration(int duration) {
         this.duration = duration;
     }
 
+    /**
+     * Définir le nombre de joueurs maximum
+     * @param maxPlayers Nombre de joueurs maximum
+     */
     public void setMax_players(int maxPlayers) {
         this.maxPlayers = maxPlayers;
     }
 
+    /**
+     * Définir si multijoueurs
+     * @param multiPlayer true si multijoueurs, false sinon
+     */
     public void setMultiPlayer(boolean multiPlayer) {
         isMultiPlayer = multiPlayer;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
+    /**
+     * Définir le nom
+     * @param name Nom
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Obtenir l'auteur
+     * @return L'auteur
+     */
     public String getAuthor(){
         return this.author;
     }
 
+    /**
+     * Obtenir le nom
+     * @return Nom
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     * Obtenir la description
+     * @return Description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Obtenir la durée
+     * @return Durée
+     */
     public int getDuration() {
         return duration;
     }
-
+    /**
+     * Obtenir le nombre de joueurs maximum
+     * @return Nombre de joueurs maximum
+     */
     public int getMax_players() {
         return maxPlayers;
     }
-
+    /**
+     * Obtenir si multijoueurs
+     * @return true si multijoueurs, false sinon
+     */
     public boolean isMultiPlayer() {
         return isMultiPlayer;
     }
 
+    /**
+     * Obtenir les données
+     * @return Données
+     */
     public HashMap<String,String> getData(){
         HashMap<String,String> data = new HashMap<>();
         data.put(NAME, this.name);
--- src/modifs/MapDataSave.java
+++ src/modifs/MapDataSave.java
@@ -4,24 +4,64 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Sauvegarde les données d'une map
+ *
+ * @author Jorys-Micke ALAÏS
+ * @author Louka DOZ
+ * @author Loic SENECAT
+ * @author Quentin RAMSAMY-AGEORGES
+ * @version 6.0 01 fevrier 2020
+ * @since 6.0
+ */
 public class MapDataSave {
 
+    /**
+     * En-tête
+     */
     private final static String HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-    private final static String START = "<DatMap>";
-    private final static String END = "</DatMap>";
+    /**
+     * Début
+     */
+    private final static String START = "<DataMap>";
+    /**
+     * Fin
+     */
+    private final static String END = "</DataMap>";
+    /**
+     * Indentation
+     */
+    private final static String INDENTATION = "  ";
 
+    /**
+     * Convertie une data et sa valeur en une chaine unique avec une syntaxe précise
+     * @return Data et sa valeur dans une syntaxe
+     */
     private static String putInSyntax(String name, String value){
         return "<" + name + ">" + value + "</" + name + ">";
     }
 
+    /**
+     * Décode le nom d'une data
+     * @return Nom de la data
+     */
     private static String getNameFromSyntax(String s){
-        return s.substring(s.indexOf("<"),s.indexOf(">")).trim();
+        return s.substring(s.indexOf("<"),s.indexOf(">")).replace("<","").trim();
     }
 
-    private static String getValueFromSyntax(String s){
-        return s.replaceAll(s.substring(s.indexOf("<"),s.indexOf(">")),"").trim();
+    /**
+     * Décode la valeur d'une data
+     * @return Valeur de la data
+     */
+    private static String getValueFromSyntax(String s, String name){
+        return s.replace("<" + name + ">","").replace("</" + name + ">","").trim();
     }
 
+    /**
+     * Ecrit les données d'une map
+     * @param data Données
+     * @throws IOException En cas d'erreur d'écriture
+     */
     public static void write(MapData data) throws IOException {
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("assets/files/data/" + data.getName() + ".tmx")));
         HashMap<String,String> attr = data.getData();
@@ -32,25 +72,33 @@ public class MapDataSave {
         writer.newLine();
 
         for(Map.Entry<String,String> entry : attr.entrySet()){
-            writer.write(MapDataSave.putInSyntax(entry.getKey(),entry.getValue()));
+            writer.write(INDENTATION + MapDataSave.putInSyntax(entry.getKey(),entry.getValue()));
             writer.newLine();
         }
 
         writer.write(END);
+        writer.close();
     }
 
+    /**
+     * Lis les données d'une map
+     * @param mapName Nom de la map
+     * @return Les données
+     * @throws IOException En cas d'erreur de lecture
+     */
     public static MapData read(String mapName) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("assets/files/data/" + mapName + ".tmx")));
-        MapData data = new MapData();
         HashMap<String,String> attr = new HashMap<>();
         String read;
 
         while((read = reader.readLine()) != null){
-            if(!read.equals(HEADER) && !read.equals(START) && !read.equals(END))
-                attr.put(MapDataSave.getNameFromSyntax(read),MapDataSave.getValueFromSyntax(read));
+            if(!read.equals(HEADER) && !read.equals(START) && !read.equals(END)){
+                String name = MapDataSave.getNameFromSyntax(read);
+                attr.put(name,MapDataSave.getValueFromSyntax(read,name));
+            }
         }
 
-        data.initFromData(attr);
-        return data;
+        reader.close();
+        return new MapData(attr);
     }
 }
